5e2cfe83c165caf06d49ed3bc923deb934f67c57
INGRNET\cmaatuk <chaim>
2021-12-08 16:45:24 +0200

    init

---
+++ include/mathlib.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include <iostream>
+
+namespace MyLib
+{
+	class MyMath
+	{
+	public:
+		static long int Add(int x, int y);
+	};
+}
+
---
+++ src/mathlib.cpp
@@ -0,0 +1,12 @@
+#include "mathlib.h"
+
+using namespace std;
+namespace MyLib
+{
+
+	long int MyMath::Add(int x, int y)
+	{
+		return x + y;
+	}
+
+}
